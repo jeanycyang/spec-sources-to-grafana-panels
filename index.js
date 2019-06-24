505514e7b8e6c7ff8e198e17bebc53048a846883
Jean Yang <bdlas321@gmail.com>
2019-06-24 18:08:39 +0800

    feat: convert swagger json to grafana json model

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const grafana = require('grafana-dash-gen');
+const swagger = require('./swagger.example');
+const apis = swagger();
 
 const Row = grafana.Row;
 const Dashboard = grafana.Dashboard;
-const Panels = grafana.Panels;
 const Graph = grafana.Panels.Graph;
 
 const dashboard = new Dashboard({
 	title: 'My Dashboard'
 });
 
-console.log(dashboard.generate());
+const rows = {};
+
+const BASE_PATH = apis.basePath;
+
+const paths = apis.paths;
+
+Object.entries(paths).forEach(([path, methods]) => {
+  Object.entries(methods).forEach(([method, content]) => {
+    if (!path.startsWith('http')) {
+      const panel = new Graph({
+        title: `${method} ${BASE_PATH}${path}`,
+        datasource: 'datasource1',
+      });
+      content.tags.forEach(tag => {
+        if (!rows[tag]) {
+          rows[tag] = new Row({
+            title: tag,
+            editable: true,
+            collapse: true,
+          });
+        }
+        rows[tag].addPanel(panel)
+      });
+    }
+  });
+});
+
+Object.values(rows).forEach(row => {
+  dashboard.addRow(row);
+});
+
+console.log(JSON.stringify(dashboard.generate()));
