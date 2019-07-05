1445be28554ca67c00440645fb0ac710df456f88
Jean Yang <bdlas321@gmail.com>
2019-07-05 16:03:23 +0800

    chore: install eslint

--- swagger-json/index.js
+++ swagger-json/index.js
@@ -1,12 +1,11 @@
 const grafana = require('grafana-dash-gen');
 const swagger = require('./swagger.example');
+
 const apis = swagger();
 
 const statusesGraph = require('../graphs/statusesGraph');
 
-const Row = grafana.Row;
-const Dashboard = grafana.Dashboard;
-const Graph = grafana.Panels.Graph;
+const { Row, Dashboard, Graph } = grafana;
 
 // Hack: use customized target object
 Graph.prototype.addTarget = function addTarget(target) {
@@ -14,7 +13,7 @@ Graph.prototype.addTarget = function addTarget(target) {
 };
 
 const dashboard = new Dashboard({
-	title: 'My Dashboard'
+  title: 'My Dashboard',
 });
 
 const rows = {};
@@ -23,12 +22,12 @@ const panels = {};
 
 const BASE_PATH = apis.basePath;
 
-const paths = apis.paths;
+const { paths } = apis;
 Object.entries(paths).forEach(([path, methods]) => {
   Object.entries(methods).forEach(([method, content]) => {
     if (!path.startsWith('http')) {
-      const filterPath = `${BASE_PATH}${path.replace(/{[a-zA-Z0-9_-]{1,}\}/g, '.*')}`
-      content.tags.forEach(tag => {
+      const filterPath = `${BASE_PATH}${path.replace(/{[a-zA-Z0-9_-]{1,}\}/g, '.*')}`;
+      content.tags.forEach((tag) => {
         const targets = [
           statusesGraph.target({
             method,
