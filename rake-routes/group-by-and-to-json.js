fe1678df714e6250aa64d658db11958c27a9cc08
Jean Yang <bdlas321@gmail.com>
2019-07-24 18:05:30 +0800

    feat: parameterize rake-routes' convert func

--- rake-routes/group-by-and-to-json.js
+++ rake-routes/group-by-and-to-json.js
@@ -2,54 +2,62 @@ const readline = require('readline');
 const fs = require('fs');
 const { once } = require('events');
 
-function getGroupNameBy(controller) {
-  const GENERAL_GROUP_FORMAT = /^([a-z_]+)[\/#]{1}/;
-  const ADMIN_GROUP_FORMAT = /^admin\/([a-z_]+)/;
-  if (controller.startsWith('admin')) {
-    const subGroup = ADMIN_GROUP_FORMAT.exec(controller)[1];
-    return subGroup;
+function groupByController(route) {
+  const GENERAL_GROUP_FORMAT = /^([a-z_]+)[/#]{1}/;
+  const groupMatch = GENERAL_GROUP_FORMAT.exec(route.controller);
+  return groupMatch[1];
+}
+
+function routeMapper(route) {
+  const mappedRoute = { ...route };
+  const parameterizedPath = route.path.replace('(.:format)', '');
+  mappedRoute.parameterizedPath = parameterizedPath;
+  const regexPath = parameterizedPath.replace(/:[a-z_]+/g, '.*');
+  mappedRoute.regexPath = regexPath;
+  return mappedRoute;
+}
+
+function railsDefaultRoutesFilter(route) {
+  if (
+    route.path.startsWith('/rails/')
+    || route.controller.startsWith('redirect')
+  ) {
+    return false;
   }
-  const groupMatch = GENERAL_GROUP_FORMAT.exec(controller);
-  const group = groupMatch[1];
-  return group;
+  return true;
 }
 
 const ROUTE_FORMAT = /^([a-z_0-9]+)?\s?([A-Z|\|]+)\s+([a-z0-9/(.:_@*)]+)\s+([a-z0-9/(.:_"#{}=>\s/,)]+)$/;
 
-const groups = {};
 
-async function convert(specFile) {
+async function convert({
+  specFile,
+  groupBy = groupByController,
+  filter = railsDefaultRoutesFilter,
+  mapper = routeMapper,
+}) {
+  const groups = {};
+
   const readInterface = readline.createInterface({
     input: fs.createReadStream(specFile),
     console: false,
   });
-
   readInterface.on('line', (line) => {
     const currentRoute = line.trim();
     const match = ROUTE_FORMAT.exec(currentRoute);
-    if (!match) {
-      return;
-    }
+    if (!match) { return; }
     const route = {
       prefix: match[1],
       verb: match[2],
       path: match[3],
       controller: match[4],
     };
-    if (
-      route.path.startsWith('/rails/')
-      || route.controller.startsWith('redirect')
-    ) {
-      return;
-    }
-    const parameterizedPath = route.path.replace('(.:format)', '');
-    route.parameterizedPath = parameterizedPath;
-    const filterPath = parameterizedPath.replace(/:[a-z_]+/g, '.*');
-    route.filterPath = filterPath;
-    const group = getGroupNameBy(route.controller);
+    if (!filter(route)) { return; }
+    const mappedRoute = mapper(route);
+    const group = groupBy(route);
     groups[group] = [
       ...(groups[group] || []),
-      route,
+      mappedRoute,
     ];
   });
 
